7528b2cff2a7ab30c596a94b8560f9d66277054e
locodavid123 <locojuancho20@hotmail.com>
2025-11-12 00:57:44 -0500

    ok

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:shitzu/page/auth_gate.dart';
-import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
